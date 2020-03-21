4f95b6d64463fe28ac9d47f4695000adc89aace8
karleee <karleee@protonmail.com>
2020-03-21 12:15:36 -0700

    Remove logger

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,8 +1,8 @@
 import { createStore, applyMiddleware } from 'redux';
 import rootReducer from '../reducers/root_reducer';
-import logger from 'redux-logger';
+// import logger from 'redux-logger';
 import thunk from '../middleware/thunk';
 
-const configureStore = () => createStore(rootReducer, applyMiddleware(thunk, logger));
+const configureStore = () => createStore(rootReducer, applyMiddleware(thunk));
 
 export default configureStore;
